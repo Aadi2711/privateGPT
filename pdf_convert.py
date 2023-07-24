73813559b99130d05fde1a65756f4863550219cb
Aviral3004 <86399898+Aviral3004@users.noreply.github.com>
2023-07-24 18:45:31 +0530

    modified

--- pdf_convert.py
+++ pdf_convert.py
@@ -1,22 +1,40 @@
-import pdf2image
+import os
+import io
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams
 
-def image_conversion(inpath,image_path):
-    print("Converting to image")
-    OUTPUT_FOLDER= None
-    FIRST_PAGE= None
-    LAST_PAGE= None
-    FORMAT= 'jpg'
-    USERPWD= None
-    USE_CROPBOX= False
-    STRICT= False
+# Function to extract text from a PDF and store it in a text file
+def extract_text_from_pdf(input_pdf_path, output_txt_folder):
 
-    pil_images= pdf2image.convert_from_path(inpath, output_folder=OUTPUT_FOLDER, first_page=FIRST_PAGE, last_page=LAST_PAGE, 
-                                            fmt=FORMAT, userpw= USERPWD, use_cropbox=USE_CROPBOX, strict=STRICT)
-    i=0
-    for image in pil_images:
-        i=i+1
-        image.save(image_path+str(i)+'image_converted.jpg')
-    
-inpath= "inpath/B224-TENDER_DOC-B224-000-81-41-CP-T-8102_A_RenditionFile.pdf"
-image_path= "image_path/"
-image_conversion(inpath, image_path)
+    output_txt_path = os.path.join(output_txt_folder, "output.txt")
+
+    with open(input_pdf_path, 'rb') as pdf_file:
+        resource_manager = PDFResourceManager()
+        fake_file_handle = io.StringIO()
+        converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams(all_texts=True))
+        page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+        with open(output_txt_path, "w", encoding="utf-8") as output_file:
+            for page in PDFPage.get_pages(pdf_file):
+                page_interpreter.process_page(page)
+
+            text = fake_file_handle.getvalue()
+            text = text.replace('\n\n', '\n')
+            output_file.write(text)
+
+        # Close the resources
+        converter.close()
+        fake_file_handle.close()
+
+def main():
+    # Path to the input PDF
+    input_pdf_path = "image_path/Pro.pdf"
+
+    # Folder to store the output text file
+    output_txt_folder = "source_documents"
+    extract_text_from_pdf(input_pdf_path, output_txt_folder)
+
+if __name__ == "__main__":
+    main()
